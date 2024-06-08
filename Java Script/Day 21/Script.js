18f4a42475aff33c715cb7b95ace4039826111db
Gaurav Gupta <gauravgupta123223@gmail.com>
2024-06-08 19:05:34 +0530

    New

---
+++ Java Script/Day 21/Script.js
@@ -0,0 +1,23 @@
+// WHAT IS JAVA SCRIPT?
+// --> 
+
+
+x=6;
+y=3;
+
+l = 25;
+w = 45;
+
+area = l*w;
+z= x+y;
+
+// console.log("Sum Of x & y =" + z);
+
+
+document.getElementById("aa").innerHTML="sum of x & y ="+z;
+
+document.getElementById("bb").innerHTML="Area Of Rectangle ="+ area;
+
+
+
+// alert("Sum of x & y =" + z)
---
+++ Java Script/Day 22/op.js
@@ -0,0 +1,81 @@
+
+//Arithmetic Operators Maths language
+
+let a = 10;
+let b = 10;
+
+console.log("a =", a, "& b =", b);
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
+
+
+
+
+//Arithmetic Operators Programming language
+
+//1) Modulos ----> % ----> a % b ---->
+
+
+//Ans only Remender Ans show......
+
+console.log("a % b =", a % b);
+
+
+//2) Exponentiation ------> power of th values (**)
+
+//example------> 2 the power of 7
+//                 2*2*2*2*2*2*2
+
+console.log("a ** b =", a ** b);
+
+
+//3) Increment ------> (++)
+
+// 
+//a++    a = 5 ----> assign
+
+// console.log(a++)  //output--- 5
+// console.log(a++)  //output--- 6
+// console.log(a++)  //output--- 7
+// console.log(a++)  //output--- 8
+// console.log(a++)  //output--- 9
+// console.log(a++)  //output--- 10............
+
+//++a      
+
+// console.log(++a)  //output--- 6
+// console.log(++a)  //output--- 7
+// console.log(++a)  //output--- 8
+// console.log(++a)  //output--- 9
+// console.log(++a)  //output--- 10
+// console.log(++a)  //output--- 11
+
+
+
+//4) Decreament-----> (--)
+
+//a--
+
+// console.log(a--)  //output--- 6
+// console.log(a--)  //output--- 7
+// console.log(a--)  //output--- 8
+// console.log(a--)  //output--- 9
+// console.log(a--)  //output--- 10
+// console.log(a--)  //output--- 11
+
+
+//--a
+
+// console.log(--a)  //output--- 10
+// console.log(--a)  //output--- 9
+// console.log(--a)  //output--- 8
+// console.log(--a)  //output--- 7
+// console.log(--a)  //output--- 6
+// console.log(--a)  //output--- 5............
+
+
+
+console.log("a*=3 =", a*=3)
+console.log("a*3 =", a*3)
