c74230d153b79de746ae603ea6c2c909175bb1ae
Gaurav Gupta <gauravgupta123223@gmail.com>
2024-07-02 19:17:14 +0530

    new

--- Java Script/Day 31/StringMethod.js
+++ Java Script/Day 31/StringMethod.js
@@ -7,7 +7,15 @@ let Name = "Gaurav Gupta";
 let NewName = str.toLowerCase();
 document.getElementById("bb").innerHTML=NewName;
 
-let nme = "Gaurav     Gupta";
-let nmestr = str.slice();
-document.getElementById("cc").innerHTML=nmestr;
+let nbr = "0744353765";
+document.getElementById("cc").innerHTML=nbr.slice(1,3);
 
+let nmme = "Gaurav Gupta";
+let nmmestr = str.trim().length;
+document.getElementById("dd").innerHTML=nmmestr;
+
+let a1 = "Ram";
+document.getElementById("ee").innerHTML=a1.replace("R","S");
+
+let b1 = "Gaurav";
+document.getElementById("ff").innerHTML=b1.replaceAll("Gaurav", "Ankush");
---
+++ Java Script/Day 33/TAsk.js
@@ -0,0 +1,25 @@
+// let user = prompt("Enter Your Username");
+
+// document.getElementById("aa").innerHTML="@"+user+user.length;
+
+
+
+
+let a = [45, 4, 9, 16, 25];
+let b = "";
+
+for(let i = 0; i<=a.length; i++)
+    {
+        b+=i+"<BR>";
+    }
+document.getElementById("bb").innerHTML=b;
+
+
+
+for(let i = 0; i<=a.length; i++)
+    {
+        if(i%2==0)
+            {
+                b+=+"<BR>"
+            }
+    }
